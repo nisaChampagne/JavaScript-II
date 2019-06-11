7e71bbb92b2c3a07ac5cb81e887ccdef3988ca59
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-06-11 16:21:39 -0400

    tried something different for challenge 1 and it worked

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,7 +56,10 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
-runners.forEach(item => fullName.push('{first_name} {last_name}'));
+runners.forEach(function (object){
+    let name = object.first_name + " " + object.last_name;
+    fullName.push(name)
+})
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
